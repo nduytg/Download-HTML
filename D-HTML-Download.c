b0f499d1d61191713ac14f36c7248274ff0a9019
Duy Nguyen <nduytg@gmail.com>
2016-03-04 17:06:29 +0700

    Added files via upload

--- D-HTML-Download.c
+++ D-HTML-Download.c
@@ -1,13 +1,20 @@
 //D-HTML-Download
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
 
 //Tham số dòng lệnh
 //Format: $ <MSSV> http://students.iitk.ac.in/courses
 int main(int argc, char **argv)
 {
 	char *page;
+	char ip[INET_ADDRSTRLEN];
 	
 	if(argc == 1)
 	{
@@ -16,8 +23,8 @@ int main(int argc, char **argv)
 	}
 	else if (argc > 2)
 	{
-		page = argv[2];
-		printf("Download: %s",argv[2]);
+		page = argv[1];
+		printf("Download: %s",argv[1]);
 	}
 	else
 	{
@@ -31,20 +38,22 @@ int main(int argc, char **argv)
 	//Make the struct empty!
 	memset(&hints, 0, sizeof(hints));
 	
-	//hints.ai_family = AF_INET;	//IPv4
-	//hints.ai_family = AF_INET6;	//IPv6
-	
 	hints.ai_family = AF_UNSPEC;	//IPv4 hay IPv6 gi cung dc
 	hints.ai_socktype = SOCK_STREAM;	//TCP for html connections
 	//hints.ai_flags = AI_PASSIVE;		//Auto fill IP
 	
-	DSock = getaddrinfo("www.vnexpress.net","http",&hints,&info);
+	DSock = getaddrinfo(argv[1],NULL,&hints,&info);
 	
 	if(DSock != 0)
 	{
 		fprintf(stderr,"Failed at getaddrinfo(): %s\n", gai_strerror(DSock));
 		exit(1);
 	}
+	//inet_ntop(AF_INET, &(sa.sin_addr), ip4, INET_ADDRSTRLEN);
+	//struct sockaddr_in *tmp = info->
+	inet_ntop(info->ai_family, &(((struct sockaddr_in *)info->ai_addr)->sin_addr),ip,INET_ADDRSTRLEN);
+	//sprintf("Dia chi cua trang %s la %s\n",page,ip);
+	printf("Dia chi cua trang: %s la %s\n",page,ip);
 				
 	//clear up struct addrinfo info!
 	freeaddrinfo(info);
